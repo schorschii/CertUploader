507c6c6c3b339602b47170d8a4699a02bdf90942
Georg Sieber <it@georg-sieber.de>
2024-04-18 12:41:26 +0200

    adjust debian package for setuptools packaging

--- installer/debian/build.sh
+++ installer/debian/build.sh
@@ -2,38 +2,44 @@
 set -e
 
 # build .deb package
+INSTALLDIR=/usr/share/certuploader
+BUILDDIR=certuploader
 
 # check root permissions
-if [ "$EUID" -ne 0 ]
-	then echo "Please run this script as root!"
-	exit
+if [ "$EUID" -ne 0 ] && ! groups | grep -q sudo ; then
+	echo "Please run this script as root!"
+	#exit 1 # disabled for github workflow. don't know why this check fails here but sudo works.
 fi
 
 # cd to working dir
 cd "$(dirname "$0")"
 
 # compile language files
-lrelease ../../lang/*.ts
+make -C ../..
 
 # empty / create necessary directories
-mkdir -p certuploader/usr/bin
-mkdir -p certuploader/usr/share/certuploader/lang
-mkdir -p certuploader/usr/share/pixmaps
-mkdir -p certuploader/usr/share/applications
-mkdir -p certuploader/etc/xdg/autostart
+if [ -d "$BUILDDIR/usr" ]; then
+    sudo rm -r $BUILDDIR/usr
+fi
 
 # copy files in place
-cp ../../certuploader.py certuploader/usr/bin/certuploader
-cp ../../lang/*.qm certuploader/usr/share/certuploader/lang
-cp ../../assets/*.png certuploader/usr/share/pixmaps
-cp ../../assets/certuploader.desktop certuploader/usr/share/applications
-cp ../../assets/certuploader-check-expiry.desktop certuploader/etc/xdg/autostart
+sudo install -D -m 644 ../../assets/*.png                             -t $BUILDDIR/usr/share/pixmaps
+sudo install -D -m 644 ../../assets/certuploader.desktop              -t $BUILDDIR/usr/share/applications
+sudo install -D -m 644 ../../assets/certuploader-check-expiry.desktop -t $BUILDDIR/etc/xdg/autostart
+sudo install -D -m 644 ../../lang/*.qm                         -t $BUILDDIR/$INSTALLDIR/lang
+sudo install -D -m 644 ../../certuploader/*.py                 -t $BUILDDIR/$INSTALLDIR/certuploader
+sudo install -D -m 644 ../../requirements.txt                  -t $BUILDDIR/$INSTALLDIR
+sudo install -D -m 644 ../../setup.py                          -t $BUILDDIR/$INSTALLDIR
+sudo install -D -m 644 ../../README.md                         -t $BUILDDIR/$INSTALLDIR
+
+# make binary available in PATH
+sudo mkdir -p $BUILDDIR/usr/bin
+sudo ln -sf   $INSTALLDIR/venv/bin/certuploader     $BUILDDIR/usr/bin/certuploader
 
 # set file permissions
-chown -R root:root certuploader
-chmod 775 certuploader/usr/bin/certuploader
+sudo chown -R root:root $BUILDDIR
 
 # build deb
-dpkg-deb -Zxz --build certuploader
+sudo dpkg-deb -Zxz --build $BUILDDIR
 
 echo "Build finished"
