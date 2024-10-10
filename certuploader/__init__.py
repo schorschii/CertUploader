fd48cc762be13e1a37ed90b529f9ba26ee601dae
Georg Sieber <it@georg-sieber.de>
2024-10-10 11:11:55 +0200

    bump version

--- certuploader/__init__.py
+++ certuploader/__init__.py
@@ -2,7 +2,7 @@ __title__     = 'CertUploader'
 __author__    = 'Georg Sieber'
 __copyright__ = '© 2021-2024'
 __license__   = 'GPL-3.0'
-__version__   = '1.3.0'
+__version__   = '1.3.1'
 __website__   = 'https://github.com/schorschii/CertUploader'
 
 __all__ = [__author__, __license__, __version__]
